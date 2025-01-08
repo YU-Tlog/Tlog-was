31d6004e3f51b12dce8017766352e4d8642ba0b8
gojungsu <gjs4565@naver.com>
2025-01-08 23:49:10 +0900

    "initiial commit"

---
+++ src/test/java/com/se/Tlog/TlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.se.Tlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

d3efdfc12832c9ca9ad95950eb51432b2234b9b0
gojungsu <gjs4565@naver.com>
2025-01-10 04:09:03 +0900

    feat: USER 엔티티 추가

---
+++ src/main/java/com/se/Tlog/domain/User/Role.java
@@ -0,0 +1,15 @@
+package com.se.Tlog.domain.User;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Role {
+
+    USER("ROLE_USER"),
+    GUEST("ROLE_GUEST"),
+    ADMIN("ROLE_ADMIN");
+
+    private final String value;
+}
---
+++ src/main/java/com/se/Tlog/domain/User/User.java
@@ -0,0 +1,45 @@
+package com.se.Tlog.domain.User;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    UUID id;
+
+    private String userId;
+    private String password;
+
+    private String name;
+    private String email;
+    private String telephoneNumber;
+    private Role role;
+
+    @Builder
+    public User(
+            String name,
+            String userId,
+            String email,
+            String telephoneNumber
+    ) {
+        this.name = name;
+        this.userId = userId;
+        //this.providerUserInfo = provider + " " + providerId;
+        this.email = email;
+        this.telephoneNumber = telephoneNumber;
+        this.role = Role.USER;
+    }
+}

50d515a9ae7f54c730b4f3c62264e031f90962de
gojungsu <gjs4565@naver.com>
2025-01-09 13:55:49 +0900

    feat: 예외 처리 기능 기초 세팅

---
+++ src/main/java/com/se/Tlog/global/response/error/ErrorRes.java
@@ -0,0 +1,14 @@
+package com.se.Tlog.global.response.error;
+
+public record ErrorRes (
+        int status,
+        String message
+){
+    public static ErrorRes of(int status , String message) {
+        return new ErrorRes(status, message);
+    }
+
+    public static ErrorRes from(ErrorType error){
+        return new ErrorRes(error.getStatusCode(), error.getMessage());
+    }
+}

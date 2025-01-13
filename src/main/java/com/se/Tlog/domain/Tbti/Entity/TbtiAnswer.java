e862ade26ac791a02cf908ab41fa78e863e21346
joongsu <112760801+joongsu@users.noreply.github.com>
2025-01-13 18:40:59 +0900

    Feture: Tbti domain 엔티티 작성 (#11)
    * feat: TbtiAnswer 엔티티 작성
    
    * feat: TbtiQuestion 엔티티 작성
    
    * feat: TbtiQuestion 성향을 위한 Enum작성

---
+++ src/main/java/com/se/Tlog/domain/Tbti/Entity/TbtiAnswer.java
@@ -0,0 +1,35 @@
+package com.se.Tlog.domain.Tbti.Entity;
+
+import com.se.Tlog.domain.User.Entity.User;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import static lombok.AccessLevel.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = PROTECTED)
+public class TbtiAnswer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "question_id")
+    private TbtiQuestion tbtiQuestion;
+
+    private int score;
+
+    @Builder
+    public TbtiAnswer(User user, TbtiQuestion tbtiQuestion, int score) {
+        this.user = user;
+        this.tbtiQuestion = tbtiQuestion;
+        this.score = score;
+    }
+}
---
+++ src/main/java/com/se/Tlog/domain/Tbti/Entity/TraitCategory.java
@@ -0,0 +1,8 @@
+package com.se.Tlog.domain.Tbti.Entity;
+
+public enum TraitCategory {
+    ACTIVITY_LEVEL, // 활동적 ↔ 여유로움
+    LOCATION_PREFERENCE, // 자연 ↔ 도시
+    PLANNING_STYLE, // 계획적 ↔ 즉흥적
+    RISK_TAKING // 개척적 ↔ 안정적
+}

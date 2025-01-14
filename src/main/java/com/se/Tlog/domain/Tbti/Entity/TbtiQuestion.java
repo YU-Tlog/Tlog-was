f44f69e4138ee6c2557864e90b0b0fc76eeccd6c
gojungsu <gjs4565@naver.com>
2025-01-15 03:29:56 +0900

    feat: 질문 생성 및 유효성 검사 메서드 작성

--- src/main/java/com/se/Tlog/domain/Tbti/Entity/TbtiQuestion.java
+++ src/main/java/com/se/Tlog/domain/Tbti/Entity/TbtiQuestion.java
@@ -1,5 +1,7 @@
 package com.se.Tlog.domain.Tbti.Entity;
 
+import com.se.Tlog.global.exception.CustomException;
+import com.se.Tlog.global.response.error.ErrorType;
 import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
@@ -26,4 +28,22 @@ public class TbtiQuestion {
         this.content = content;
         this.traitCategory = traitCategory;
     }
+
+    // 질문 생성 메서드
+    public static TbtiQuestion creation(String content, TraitCategory traitCategory) {
+        return TbtiQuestion.builder()
+                .content(content)
+                .traitCategory(traitCategory)
+                .build();
+    }
+
+    // 유효성 검사 메서드
+    private void validateContent(String content){
+        if(content == null || content.isBlank()){
+            throw new CustomException(ErrorType.CONTENT_NOT_FOUND);
+        }
+        // 필요한 검사가 있다면 지속적으로 추가할 예정!
+    }
+
+
 }

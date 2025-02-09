d131383323b1a7905a48f3db707b77033fddf67d
gojungsu <gjs4565@naver.com>
2025-02-09 10:28:26 +0900

    feat: Tag Repository 작성

--- src/main/java/com/se/Tlog/domain/Travel/repository/TagRepository.java
+++ src/main/java/com/se/Tlog/domain/Travel/repository/TagRepository.java
@@ -1,10 +1,14 @@
 package com.se.Tlog.domain.Travel.repository;
 
 import com.se.Tlog.domain.Travel.Entity.Tag;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface TagRepository extends JpaRepository<Tag, Long> {
+import java.util.Optional;
 
+@Repository
+public interface TagRepository extends MongoRepository<Tag, String> {
+    Optional<Tag> findByName(String name);
+
+    boolean existsByName(String name);
 }

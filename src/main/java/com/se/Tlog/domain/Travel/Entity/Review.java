72e4f8f0427b6dd069101e611dda6f7f09d74a21
gojungsu <gjs4565@naver.com>
2025-02-09 10:27:50 +0900

    feat: Review document 작성

--- src/main/java/com/se/Tlog/domain/Travel/Entity/Review.java
+++ src/main/java/com/se/Tlog/domain/Travel/Entity/Review.java
@@ -1,51 +1,31 @@
 package com.se.Tlog.domain.Travel.Entity;
 
-import com.se.Tlog.domain.User.Entity.User;
-import io.micrometer.common.lang.NonNull;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
+
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
 
-@Entity
+@Document(collection = "reviews")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Review {
+
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	
-	@ManyToOne
-	@JoinColumn(name = "userId")
-	private User user;
-	
-	@ManyToOne
-	@JoinColumn
-	private Destination destination;
-	
+	private String id;
+
+	private String userId;
+	private String destinationId;
+
 	private int rating;
-	
 	private String content;
-	
-	/*
-	@OneToMany(mappedBy = "")
-	private List<Media> media;
-	*/ 
-	
+
 	@Builder
-	public Review(
-			@NonNull User user,
-			@NonNull Destination destination,
-			int rating,
-			@NonNull String content) {
-		this.user = user;
-		this.destination = destination;
+	public Review(String userId, String destinationId, int rating, String content) {
+		this.userId = userId;
+		this.destinationId = destinationId;
 		this.rating = rating;
 		this.content = content;
 	}

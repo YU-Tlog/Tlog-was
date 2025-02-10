e838e1e8fd65b1f0bebf8848b54dd5cfa4e01331
patternplane <bss011202@naver.com>
2025-02-10 17:59:24 +0900

    feat: 외부 소셜 로그인과 관련된 성공/실패 상태 추가
    SuccessType
    - LOGIN_SSO_SUCCESS : 소셜 로그인 성공 (status : 200)
    
    ErrorType
    - SSO_LOGIN_FAIL : 소셜 로그인 실패 또는 취소 (status : 400)
    - SSO_ACCESSTOKEN_FAIL : 로그인 후 인증 토큰 발급 실패 (status : 500)
    - UNSUPPORTED_SSO_LOGIN : 구현되지 않은 소셜 로그인 방식 (status : 501)

--- src/main/java/com/se/Tlog/global/response/error/ErrorType.java
+++ src/main/java/com/se/Tlog/global/response/error/ErrorType.java
@@ -11,6 +11,9 @@ public enum ErrorType {
     // 400 잘못된 요청
     CONTENT_NOT_FOUND(HttpStatus.BAD_REQUEST, "Content 내용이 비어있습니다."),
     ROLE_MISMATCH(HttpStatus.BAD_REQUEST,"Role 값을 잘못 입력하였습니다."),
+    
+    // 사용자로부터 소셜 로그인 인증 실패
+    SSO_LOGIN_FAIL(HttpStatus.BAD_REQUEST,"외부 소셜 로그인이 취소되거나 실패했습니다."),
 
     // 인증
     // 401
@@ -30,7 +33,13 @@ public enum ErrorType {
     ALREADY_EXISTS_DESTINATION(HttpStatus.CONFLICT, "이미 존재하는 여행지입니다."),
     // 서버 에러
     // 500
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "내부 서버 에러. 서버 팀으로 연락주시기 바랍니다.");
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "내부 서버 에러. 서버 팀으로 연락주시기 바랍니다."),
+    
+    // 외부 소셜 로그인 처리 중 에러
+    SSO_ACCESSTOKEN_FAIL(HttpStatus.INTERNAL_SERVER_ERROR, "외부 인증 서버로부터 인증을 받는데 실패했습니다."),
+
+    // 501 구현되지 않은 기능
+    UNSUPPORTED_SSO_LOGIN(HttpStatus.NOT_IMPLEMENTED, "현재 해당 소셜 로그인 방식은 아직 지원되지 않습니다.");
 
 
     private final HttpStatus status;
